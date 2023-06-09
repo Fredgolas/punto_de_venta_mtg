25055233d6bd7491273771521cfe861bc2f1d5b7
Fredgolas <fredy00roca@gmail.com>
2023-06-08 21:19:19 -0600

    User models basic setup

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Cliente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    celular = models.CharField(max_length=10)
+    saldo = models.DecimalField(default=0)
+
+class Empleado(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
