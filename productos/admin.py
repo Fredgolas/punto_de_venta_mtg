95f1c8105784b43900d3e9b924d0fb05ef5e346a
Alfredo Pérez <fredy00roca@gmail.com>
2023-06-09 03:08:54 -0600

    Enable django admin

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from productos.models import Card
 
 # Register your models here.
+class CardAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Card, CardAdmin)
